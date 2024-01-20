ab3a6c9ef394c62e67266ce496e38013cdbd9c10
martinpfh <martin@pfh.de>
2024-01-20 21:10:36 +0100

    introduce style parser hashmap

--- src/quoteparser.rs
+++ src/quoteparser.rs
@@ -1,15 +1,24 @@
-use std::convert::Infallible;
 //
 // Quote Parser
 //
 use std::str::FromStr;
+use std::collections::HashMap;
 use crate::regex::Regex;
 use crate::quotestyle::QuoteStyle;
 use crate::error::ParseError;
 
-#[derive(Debug, PartialEq, Default)]
+// type QuoteParser = for<'a, 'b, 'c> fn(&'a str, &'b str, &'c str) -> Result<Quote, ParseError>;
+
+#[derive(Debug, Default)]
 struct Quote {
     price: f64,
+    styleparser: HashMap<QuoteStyle, String>,
+}
+
+impl PartialEq for Quote {
+    fn eq(&self, other: &Self) -> bool {
+        self.price == other.price
+    }
 }
 impl FromStr for Quote {
     type Err = ParseError;
@@ -18,9 +27,20 @@ impl FromStr for Quote {
     }
 }
 impl Quote {
+    fn parse_tresury_price(number: &str, fraction: &str, fraction32: &str) -> Result<Self, ParseError> {
+        Ok(Quote::default())
+    }
+
+    fn new() -> Self {
+        let mut styleparser = HashMap::new();
+
+        styleparser.insert(QuoteStyle::Bond, String::from("Bond"));
+
+        Quote { price: 0.0, styleparser }
+    }
     fn parse(s: &str, quotestyle: QuoteStyle) -> Result<Self, ParseError> {
         if let Ok(result) = s.parse::<f64>() {
-            return Ok(Quote { price: result });
+            return Ok(Quote::default());
         }
 
         let re = Regex::new(r"(?P<number>^\d+)(?P<delimiter_frac>[\.\-\'])?(?P<fraction>\d{2})?(?P<delimiter32>\'?)(?P<fraction32>[\d+,\+])?").unwrap();
@@ -29,35 +49,47 @@ impl Quote {
             return Err(ParseError::UnexpectedToken);
         };
 
-        let _number: i32 = match captures.name("number") {
+        let number: i32 = match captures.name("number") {
             Some(number_str) => number_str.as_str().parse::<i32>().unwrap(),
             None => 0,
         };
 
-        let _delimiter32 = match captures.name("delimiter32") {
-            Some(delimiter32_str) => delimiter32_str.as_str(),
+        let delimiter_frac: &str = match captures.name("delimiter_frac") {
+            Some(delimiter_frac_str) => delimiter_frac_str.as_str(),
             None => "",
         };
 
-        let _fraction: i32 = match captures.name("fraction") {
+        let fraction: i32 = match captures.name("fraction") {
             Some(fraction_str) => fraction_str.as_str().parse::<i32>().unwrap(),
             None => 0,
         };
 
-        let _fraction32: &str = match captures.name("fraction32") {
-            Some(fraction32_str) => fraction32_str.as_str(),
-            None => ""
-        };
-
-        let _delimiter32: &str = match captures.name("delimiter32") {
+        let delimiter32 = match captures.name("delimiter32") {
             Some(delimiter32_str) => delimiter32_str.as_str(),
             None => "",
         };
 
-        let _delimiter_frac: &str = match captures.name("delimiter_frac") {
-            Some(delimiter_frac_str) => delimiter_frac_str.as_str(),
-            None => "",
+        let fraction32: &str = match captures.name("fraction32") {
+            Some(fraction32_str) => fraction32_str.as_str(),
+            None => ""
         };
+
+        let style = if quotestyle == QuoteStyle::Detect { QuoteStyle::detect(fraction32, delimiter_frac, delimiter32) } else { quotestyle };
+
+
+        // if style == QuoteStyle::Bond {
+        //     let mut price = _number as f64;
+        //     price += _fraction as f64 / 100.0;
+        //     price += _fraction32.replace(",", ".").parse::<f64>().unwrap() / 100.0;
+        //     return Ok(Quote { price: price });
+        // }
+
+        // if style == QuoteStyle::Stock {
+        //     let mut price = _number as f64;
+        //     price += _fraction as f64 / 100.0;
+        //     price += _fraction32.replace(",", ".").parse::<f64>().unwrap() / 100.0;
+        //     return Ok(Quote { price: price });
+        // }
 
         return Ok(Quote::default());
     }
--- src/quotestyle.rs
+++ src/quotestyle.rs
@@ -2,9 +2,10 @@
 // QuoteStyle
 //
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub enum QuoteStyle {
     Detect,
+    Decimal,
     Bond,
     ShortNoteFuture,
     BondFuture,
@@ -13,11 +14,21 @@ pub enum QuoteStyle {
 impl QuoteStyle {
     pub fn detect(fraction32: &str, delimiter_frac: &str, delimiter32: &str) -> QuoteStyle {
         if fraction32.contains(".") {
+            QuoteStyle::Decimal
+        } else if fraction32.contains("+") {
             QuoteStyle::Bond
-        } else if fraction32.contains(" ") {
-            QuoteStyle::ShortNoteFuture
-        } else {
+        } else if delimiter32.contains("'") && delimiter32.is_empty() {
             QuoteStyle::BondFuture
+        } else if delimiter32.contains("'") && delimiter32.contains("'") {
+            if fraction32.contains("1") || fraction32.contains("6") || fraction32.contains("8") {
+                return QuoteStyle::ShortNoteFuture  
+            } else if fraction32.contains("2") || fraction32.contains("5") || fraction32.contains("7") {
+                return QuoteStyle::ShortNoteFuture
+            } else {
+                return QuoteStyle::Bond
+            }
+        } else {
+            QuoteStyle::Bond
         }
     }
 }
@@ -25,9 +36,16 @@ impl QuoteStyle {
 mod tests {
     #[test]
     fn test_detect() {
+        // (None, None, None, QuoteStyle.BOND), # Default
+        // ('', '', '', QuoteStyle.BOND), # Default
+        // ('325', '.', None, QuoteStyle.DECIMAL), # Decimal
+        // ('+', '', '', QuoteStyle.BOND), # 108-04+
+        // ('2', '\'', '', QuoteStyle.BOND_FUTURE), # 108'182
         use super::QuoteStyle;
-        assert_eq!(QuoteStyle::detect("1/1", "/", ""), QuoteStyle::Bond);
-        assert_eq!(QuoteStyle::detect("1/1", "/", " "), QuoteStyle::ShortNoteFuture);
-        assert_eq!(QuoteStyle::detect("1/1", "/", "="), QuoteStyle::BondFuture);
+        assert_eq!(QuoteStyle::detect("", "", ""), QuoteStyle::Bond);
+        assert_eq!(QuoteStyle::detect("325", ".", ""), QuoteStyle::Decimal);
+        assert_eq!(QuoteStyle::detect("+", "", ""), QuoteStyle::Bond);
+        assert_eq!(QuoteStyle::detect("2", "'", ""), QuoteStyle::BondFuture);
+        assert_eq!(QuoteStyle::detect("1", "'", "'"), QuoteStyle::ShortNoteFuture);
     }
 }
