d6f31c25f66913611b6dad9702c6abd32bbe2895
martinpfh <martin@pfh.de>
2024-01-21 18:57:08 +0100

    Remove some error values that are not neeed

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,12 @@
 //!crate error
 
-// #![allow(unused)] // for starting project only, remove later.
+#![allow(unused)] // for starting project only, remove later.
 
 #[derive(Debug, PartialEq)]
 pub enum ParseError {
-    InvalidCharacter,
     InvalidNumber,
     InvalidString,
     InvalidSymbol,
-    InvalidToken,
     InvalidType,
-    MissingToken,
     UnexpectedToken,
 }
