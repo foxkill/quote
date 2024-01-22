143a2d5ff5de2bd18e7490a3c4e612ef87fcbd59
martinpfh <martin@pfh.de>
2024-01-22 17:07:20 +0100

    Use only one parse function which uses different maps.

--- src/styleparsers.rs
+++ src/styleparsers.rs
@@ -10,6 +10,8 @@ use std::collections::HashMap;
 use crate::error::ParseError;
 use lazy_static::lazy_static;
 
+type FractionMap = HashMap<char, f64>;
+
 macro_rules! hashmap {
     ($($key:expr => $value:expr),*) => ({
         let mut map = HashMap::new();
@@ -18,7 +20,7 @@ macro_rules! hashmap {
     });
 }
 lazy_static! {
-    static ref FRACTION_BOND: HashMap<char, f64> = hashmap!(
+    static ref FRACTION_BOND: FractionMap = hashmap!(
         '0' => 0.0,
         '1' => 1.0/8.0,
         '2' => 1.0/4.0,
@@ -45,72 +47,42 @@ lazy_static! {
     static ref FRACTION32_NOTE: HashMap<char, f64> = hashmap!(
         '+' => 0.0,
         '0' => 0.0,
-        '1' => 0.25,
-        '2' => 0.5,
-        '3' => 0.75
+        '2' => 0.25,
+        '5' => 0.5,
+        '7' => 0.75
     );
 }
 
-pub fn parse_treasury_price(number: &str, fraction: &str, fraction32: &str) -> Result<f64, ParseError> {
+fn parse_quote(number: &str, fraction: &str, fraction32: &str, m: &FractionMap) -> Result<f64, ParseError> {
     let Ok(price) = number.parse::<f64>() else {
-        return Err(ParseError::InvalidNumber);
+        return Err(ParseError::Number);
     };
 
     let Ok(fraction) = fraction.parse::<f64>() else {
-        return Err(ParseError::InvalidNumber);
+        return Err(ParseError::Number);
     };
 
-    let fr32 = FRACTION_BOND.get(
+    let fr32 = m.get(
         &fraction32.chars().next().unwrap_or('0')
     ).unwrap_or(&0.0);
 
     Ok(price + ((fraction + fr32)/32.0))
 }
+pub fn parse_treasury_price(number: &str, fraction: &str, fraction32: &str) -> Result<f64, ParseError> {
+    parse_quote(number, fraction, fraction32, &FRACTION_BOND)
+}
 
 pub fn parse_short_term_note_future_price(number: &str, fraction: &str, fraction32: &str) -> Result<f64, ParseError> {
-    let Ok(price) = number.parse::<f64>() else {
-        return Err(ParseError::InvalidNumber);
-    };
-
-    let Ok(fraction) = fraction.parse::<f64>() else {
-        return Err(ParseError::InvalidNumber);
-    };
-
-    let c = fraction32.chars().next().unwrap_or('0');
-    let fr32 = FRACTION32_SHORT_TERM_NOTE.get(&c).unwrap_or(&0.0);
-
-    Ok(price + ((fraction + fr32)/32.0))
+    parse_quote(number, fraction, fraction32, &FRACTION32_SHORT_TERM_NOTE)
 }
 
 /// Parse the price of a note future, ZF, ZN, TN
 pub fn parse_note_future_price(number: &str, fraction: &str, fraction32: &str) -> Result<f64, ParseError> {
-    let Ok(price) = number.parse::<f64>() else {
-        return Err(ParseError::InvalidNumber);
-    };
-
-    let Ok(fraction) = fraction.parse::<f64>() else {
-        return Err(ParseError::InvalidNumber);
-    };
-
-    let c = fraction32.chars().next().unwrap_or('0');
-    let fr32 = FRACTION32_NOTE.get(&c).unwrap_or(&0.0);
-
-    Ok(price + ((fraction + fr32)/32.0))
+    parse_quote(number, fraction, fraction32, &FRACTION32_NOTE)
 }
 
-
 pub fn parse_bond_future_price(number: &str, fraction: &str, fraction32: &str) -> Result<f64, ParseError> {
-    let Ok(price) = number.parse::<f64>() else {
-        return Err(ParseError::InvalidNumber);
-    };
-
-    let Ok(fraction) = fraction.parse::<f64>() else {
-        return Err(ParseError::InvalidNumber);
-    };
-
-    let _ = fraction32;
-
-    Ok(price + (fraction/32.0))
+    parse_quote(number, fraction, "0", &FRACTION32_NOTE)
 }
 
 
