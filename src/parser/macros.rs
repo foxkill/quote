634bb94f23a21b8a288b3a59a4785911cff18996
martinpfh <martin@pfh.de>
2024-01-26 20:48:39 +0100

    Put hashmap macros into macros.rs file.

--- src/parser/macros.rs
+++ src/parser/macros.rs
@@ -1,10 +1,19 @@
 //! Contains all macros for quote parser library.
 //!
 //! 
- 
+#![allow(unused_macros)] 
+
 #[macro_export]
 macro_rules! extract_capture {
     ($caps:expr, $($name:ident), *) => {
         $(let $name: &str = $caps.name(stringify!($name)).map_or("", |m| m.as_str());)*
     };
+}
+
+macro_rules! hashmap {
+    ($($key:expr => $value:expr),*) => ({
+        let mut map = HashMap::new();
+        $(map.insert($key, $value);)*
+        map
+    });
 }
