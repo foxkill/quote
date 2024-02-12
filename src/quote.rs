9a277f25fc33c2f961ccebdf98bc0ee7d233c07c
martinpfh <martin@pfh.de>
2024-02-12 19:29:51 +0100

    Add additional test.

--- src/quote.rs
+++ src/quote.rs
@@ -1,5 +1,6 @@
 //! Quote parser as struct implementation.
 
+use std::convert::From;
 use std::str::FromStr;
 
 use crate::{
@@ -46,13 +47,28 @@ impl From<&str> for Quote {
     }
 }
 
+impl From<f64> for Quote {
+    fn from(value: f64) -> Self {
+        Self(value)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
     use super::*;
     #[test]
     fn it_should_apply_parse_on_a_str() {
         let expected = 126.78125;
         let result: f64 = "126'25".parse::<Quote>().unwrap().into();
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn it_should_apply_parse_on_a_string() {
+        let expected = 126.78125;
+        let quote: Quote = "126'25".parse().unwrap();
+        let result: f64 = quote.into();
         assert_eq!(expected, result);
     }
 
@@ -64,9 +80,8 @@ mod tests {
     }
 
     #[test]
-    fn from() {
+    fn it_shoud_support_the_from_trait() {
         let result: f64 = Quote::from("126'25").into();
-        // dbg!(q);
         assert_eq!(126.78125, result);
     }
 }
